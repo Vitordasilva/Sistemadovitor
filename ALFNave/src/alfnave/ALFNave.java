cd101eb55915f8df47b75b30188261bd4ff85f71
pc-asus <pc-asus@Vitor>
2015-11-19 14:36:41 -0200

    Alteração da mensagem de usuario ou senha incorreta

---
+++ ALFNave/src/alfnave/ALFNave.java
@@ -0,0 +1,13 @@
+
+package alfnave;
+
+import alfnave.visualizacao.Telainicial;
+
+public class ALFNave {
+
+    public static void main(String[] args) {
+        Telainicial telainicial = new Telainicial();
+        telainicial.setVisible(true);
+    }
+    
+}
